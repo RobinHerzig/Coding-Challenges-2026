cab6b0d1490fadfc8a65fc37b83aac14ac46957a
Robin H <robin.hrzg@gmail.com>
2026-01-24 20:35:15 -0600

    Balanced binary tree. (Binary tree)

---
+++ 01242026.ts
@@ -0,0 +1,25 @@
+// Balanced binary tree. (Binary tree)
+
+function isBalanced(root: TreeNode | null): boolean {
+  let result = true;
+
+  (function dfs(node: TreeNode | null): number {
+    if (!node || !result) {
+      return 0;
+    }
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+    result = result && Math.abs(left - right) <= 1;
+
+    return Math.max(left, right) + 1;
+  })(root);
+
+  return result;
+}
+
+// https://leetcode.com/problems/balanced-binary-tree/
+
+// root: TreeNode | null. 0 <= number of nodes <= 5000. -10^4 <= Node.val <= 10^4.
+// Return true if the binary tree is height-balanced.
+// - A height-balanced binary tree is one in which the depth of the two subtrees of every node never differs by more than one.
