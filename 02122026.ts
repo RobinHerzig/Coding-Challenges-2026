05f4fc054025dc4c762083334e43217f5ccf8a7c
Robin H <robin.hrzg@gmail.com>
2026-02-12 13:15:02 -0600

    Reorder list. (Linked list)

--- 02122026.ts
+++ 02122026.ts
@@ -1,7 +1,46 @@
 // Reorder list. (Linked list)
 
 function reorderList(head: ListNode | null): void {
-  
+  // Step 1: Split linked list in half.
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  let left = head;
+  let right = slow.next;
+  slow.next = null;
+
+  // Step 2: Reverse second half.
+  let prev = null;
+  let curr = right;
+
+  while (curr) {
+    const next = curr.next;
+
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  right = prev;
+
+  // Step 3: Recombine lists.
+  while (right) {
+    const nextLeft = left.next;
+    const nextRight = right.next;
+
+    left.next = right;
+    right.next = nextLeft;
+
+    left = nextLeft;
+    right = nextRight;
+  }
+
+  return undefined;
 }
 
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] => [1, 10, 2, 9, 3, 8, 4, 7, 5, 6]
