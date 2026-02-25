58aade4b76dc94d51150b15882d3e8c655d77789
Robin H <robin.hrzg@gmail.com>
2026-02-24 20:37:21 -0600

    Find minimum in rotated sorted array. (Binary search)

---
+++ 02242026.ts
@@ -0,0 +1,29 @@
+// Find minimum in rotated sorted array. (Binary search)
+
+function findMin(nums: number[]): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if (nums[middle] > nums.at(-1)!) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+
+  return nums[left];
+}
+
+console.log(findMin([3, 4, 5, 1, 2]), 1);
+console.log(findMin([4, 5, 6, 7, 0, 1, 2]), 0);
+console.log(findMin([11, 13, 15, 17]), 11);
+console.log(findMin([3, 1, 2]), 1);
+console.log(findMin([5, 1, 2, 3, 4]), 1);
+console.log(findMin([1]), 1);
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+// nums: number[]. 1 <= nums.length <= 5000. -5000 <= nums[i] <= 5000. All integers are unique.
+// Return the minimum element of the sorted array in O(log n) time complexity.
