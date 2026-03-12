19ae042afd3d99566d9ed35b68f7efde7c2c0ea1
Robin H <robin.hrzg@gmail.com>
2026-03-11 19:07:09 -0500

    Permutation in str2. (Sliding window)

---
+++ 03112026.ts
@@ -0,0 +1,53 @@
+// Permutation in str2. (Sliding window)
+
+function checkInclusion(str1: string, str2: string): boolean {
+  const map = new Map<string, number>(); // <char, count>;
+  let left = 0;
+  let right = 0;
+
+  // Step 1: Populate map with chars from str1.
+  [...str1].forEach((char) => map.set(char, (map.get(char) || 0) + 1));
+
+  // Step 2: Sliding window algorithm to add and remove chars from map.
+  while (right < str2.length) {
+    // Cache right pointer.
+    map.set(str2[right], (map.get(str2[right]) || 0) - 1);
+
+    if (map.get(str2[right]) === 0) {
+      map.delete(str2[right]);
+    }
+
+    // Increment left pointer.
+    if (right - left + 1 > str1.length) {
+      map.set(str2[left], (map.get(str2[left]) || 0) + 1);
+
+      if (map.get(str2[left]) === 0) {
+        map.delete(str2[left]);
+      }
+
+      left++;
+    }
+
+    // Check window condition.
+    if (map.size === 0) {
+      return true;
+    }
+
+    // Increment right pointer.
+    right++;
+  }
+
+  return false;
+}
+
+console.log(checkInclusion("ab", "eidbaooo"), true);
+console.log(checkInclusion("ab", "eidboaoo"), false);
+console.log(checkInclusion("bc", "abcd"), true);
+console.log(checkInclusion("cb", "abcd"), true);
+console.log(checkInclusion("bd", "abcd"), false);
+console.log(checkInclusion("ba", "abcd"), true);
+// https://leetcode.com/problems/permutation-in-string/
+
+// str1: string. 1 <= str1.length <= 10^4. Lower English letters.
+// str2: string. 1 <= str2.length <= 10^4. Lower English letters.
+// Return true if str2 contains a permutation of str1.
