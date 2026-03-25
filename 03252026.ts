bb0353488c81fcd2cc0b1de6d3eb534e9eb81f86
Robin H <robin.hrzg@gmail.com>
2026-03-25 12:21:12 -0500

    Kth smallest element in a BST. (Binary tree)

---
+++ 03252026.ts
@@ -0,0 +1,30 @@
+// Kth smallest element in a BST. (Binary tree)
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+  let result = 0;
+
+  (function dfs(node: TreeNode | null): void {
+    if (!node || k < 0) {
+      return undefined;
+    }
+
+    // In-place retreival
+    dfs(node.left);
+
+    if (--k === 0) {
+      result = node.val;
+    }
+
+    dfs(node.right);
+
+    return undefined;
+  })(root);
+
+  return result;
+}
+
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+// root: TreeNode | null. k <= number of nodes <= 10^4. 0 <= Node.val <= 10^4.
+// k: number. 1 <= k <= number of nodes.
+// Return the kth smallest value (1-indexed) of all nodes in the tree.
