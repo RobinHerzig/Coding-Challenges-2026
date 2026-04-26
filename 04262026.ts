66435f33f1bc0344dbd3ff58cb57fa8223a63c86
Robin H <robin.hrzg@gmail.com>
2026-04-26 12:51:39 -0500

    Return all possible subsets. (Backtracking)

---
+++ 04262026.ts
@@ -0,0 +1,32 @@
+// Return all possible subsets. (Backtracking)
+
+function subsets(nums: number[]): number[][] {
+  const result: number[][] = [];
+  let i = 0;
+
+  // Backtracking algorithm
+  (function dfs(set: number[], i: number): void {
+    // Base case
+    if (i === nums.length) {
+      result.push(set);
+
+      return undefined;
+    }
+
+    // Recursive case
+    dfs([...set], i + 1);
+    set.push(nums[i]);
+    dfs([...set], i + 1);
+
+    return undefined;
+  })([], 0);
+
+  return result;
+}
+
+console.log(subsets([1, 2, 3]), [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]);
+console.log(subsets([0]), [[], [0]]);
+// https://leetcode.com/problems/subsets/
+
+// nums: number[]. 1 <= nums.length <= 10. -10 <= nums[i] <= 10. All numbers are unique.
+// Return an array of all possible subsets. Do not include duplicate sets.
