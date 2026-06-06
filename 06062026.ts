01f1e058e8556532e23cb0bc1990fe999ef3416b
Robin H <robin.hrzg@gmail.com>
2026-06-06 12:40:00 -0500

    // Count good nodes. (Binary tree)

---
+++ 06062026.ts
@@ -0,0 +1,25 @@
+// Count good nodes. (Binary tree)
+
+function goodNodes(root: TreeNode | null): number {
+  function dfs(node: TreeNode | null, max: number): number {
+    if (!node) {
+      return 0;
+    }
+
+    const isGood = node.val >= max;
+    max = Math.max(max, node.val);
+
+    const left = dfs(node.left, max);
+    const right = dfs(node.right, max);
+
+    return Number(isGood) + left + right;
+  }
+
+  return dfs(root, root.val);
+}
+
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+// root: TreeNode | null. 1 <= number of nodes <= 10^5. -10^4 <= Node.val <= 10^4.
+// Return the number of good nodes in the tree.
+// - A good node is one where there are no greater values along the path from the root.
