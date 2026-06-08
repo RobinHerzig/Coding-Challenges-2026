53b48bfcb42e0809978997a9d1754a13b6d5b3f8
Robin H <robin.hrzg@gmail.com>
2026-06-08 09:17:57 -0500

    Longest repeating character replacement. (Sliding window)

---
+++ 06082026.ts
@@ -0,0 +1,42 @@
+// Longest repeating character replacement. (Sliding window)
+
+function characterReplacement(str: string, k: number): number {
+  const map = new Map<string, number>();
+  let maxFreq = 0;
+  let maxLength = 0;
+  let left = 0;
+  let right = 0;
+
+  while (right < str.length) {
+    // Cache right-pointer value.
+    const char = str[right];
+    map.set(char, (map.get(char) || 0) + 1);
+    maxFreq = Math.max(maxFreq, map.get(char)!);
+
+    // Check window condition.
+    while (right - left + 1 - maxFreq > k) {
+      // Increment left pointer.
+      const leftChar = str[left];
+      map.set(leftChar, map.get(leftChar)! - 1);
+      left++;
+    }
+
+    // Update maxLength.
+    maxLength = Math.max(maxLength, right - left + 1);
+
+    // Increment right pointer.
+    right++;
+  }
+
+  return maxLength;
+}
+
+console.log(characterReplacement("ABAB", 2), 4);
+console.log(characterReplacement("AABABBA", 1), 4);
+console.log(characterReplacement("ABABC", 0), 1);
+console.log(characterReplacement("ABCCCA", 2), 5);
+// https://leetcode.com/problems/longest-repeating-character-replacement/
+
+// str: string. 1 <= str.length <= 10^5. Uppercase English letters.
+// k: number. 0 <= k <= str.length. Number of characters that can be changed.
+// Return the length of the longest substring possible changing, at most, k characters.
