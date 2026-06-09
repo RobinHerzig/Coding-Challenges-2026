429b0c22176894a0a26afe001f89b7788b1272d7
Robin H <robin.hrzg@gmail.com>
2026-06-09 18:50:29 -0500

    Generate all valid combinations of parentheses. (Backtracking)

---
+++ 06092026.ts
@@ -0,0 +1,35 @@
+// Generate all valid combinations of parentheses. (Backtracking)
+
+function combinationSum(candidates: number[], target: number): number[][] {
+  const result: number[][] = [];
+  candidates.sort((a, b) => a - b);
+
+  (function dfs(set: number[], sum: number, i: number): void {
+    if (sum === target) {
+      result.push([...set]);
+      return undefined;
+    }
+
+    for (let j = i; j < candidates.length; j++) {
+      const candidate = candidates[j];
+
+      if (sum + candidate > target) {
+        return undefined;
+      }
+
+      set.push(candidate);
+      dfs(set, sum + candidate, j);
+      set.pop();
+    }
+  })([], 0, 0);
+
+  return result;
+}
+
+console.log(combinationSum([2, 3, 6, 7], 7), [[2, 2, 3], [7]]);
+console.log(combinationSum([2], 1), []);
+// https://leetcode.com/problems/combination-sum/
+
+// candidates: number[]. 1 <= candidates.length <= 30. 2 <= candidates[i] <= 40. All elements are distinct.
+// target: number. 1 <= target <= 40.
+// Return an array of all unique number combinations that add to the target. Numbers can be used more than once.
