dd52bc8226c3ffb7d263a873426a2d915182c42f
Robin H <robin.hrzg@gmail.com>
2026-06-10 18:52:23 -0500

    Combination sum II. (Backtracking)

---
+++ 06102026.ts
@@ -0,0 +1,45 @@
+// Combination sum II. (Backtracking)
+
+function combinationSum2(candidates: number[], target: number): number[][] {
+  const result: number[][] = [];
+  candidates.sort((a, b) => a - b);
+
+  (function dfs(set: number[], sum: number, i: number): void {
+    if (sum === target) {
+      result.push([...set]);
+      return undefined;
+    }
+
+    for (let j = i; j < candidates.length; j++) {
+      const candidate = candidates[j];
+
+      if (j > i && candidate === candidates[j - 1]) {
+        continue;
+      }
+
+      if (sum + candidate > target) {
+        return undefined;
+      }
+
+      set.push(candidate);
+      dfs(set, sum + candidate, j + 1);
+      set.pop();
+    }
+  })([], 0, 0);
+
+  return result;
+}
+
+console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8), [
+  [1, 1, 6],
+  [1, 2, 5],
+  [1, 7],
+  [2, 6],
+]);
+console.log(combinationSum2([2], 1), []);
+console.log(combinationSum2([2, 5, 2, 1, 2], 5), [[1, 2, 2], [5]]);
+// https://leetcode.com/problems/combination-sum-ii/
+
+// candidates: number[]. 1 <= candidates.length <= 100. 2 <= candidates[i] <= 50. All elements are distinct.
+// target: number. 1 <= target <= 30.
+// Return an array of all unique number combinations that add to the target. Numbers can be used once per occurance.
