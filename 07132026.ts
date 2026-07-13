19ab64f610640a75363348369c35d3d4947e81d1
Robin H <robin.hrzg@gmail.com>
2026-07-13 13:37:41 -0500

    Combination sum. (Backtracking)

---
+++ 07132026.ts
@@ -0,0 +1,40 @@
+// Combination sum. (Backtracking)
+
+function combinationSum(candidates: number[], target: number): number[][] {
+  const result: number[][] = [];
+  candidates.sort((a, b) => a - b);
+
+  (function dfs(set: number[], sum: number, i: number): void {
+    if (sum === target) {
+      result.push([...set]);
+      return undefined;
+    }
+
+    for (let j = i; j < candidates.length; j++) {
+      const candidate = candidates[j];
+
+      if (candidate + sum > target) {
+        break;
+      }
+
+      // Continue with candidate
+      set.push(candidate);
+      dfs(set, sum + candidate, j);
+
+      // Continue without candidate
+      set.pop();
+    }
+
+    return undefined;
+  })([], 0, 0);
+
+  return result;
+}
+
+console.log(combinationSum([2, 3, 6, 7], 7), [[2, 2, 3], [7]]);
+console.log(combinationSum([2], 1), []);
+// https://leetcode.com/problems/combination-sum/
+
+// candidates: number[]. 1 <= candidates.length <= 30. 2 <= candidates[i] <= 40. All elements are distinct.
+// target: number. 1 <= target <= 40.
+// Return an array of all unique number combinations that add to the target. Numbers can be used more than once.
