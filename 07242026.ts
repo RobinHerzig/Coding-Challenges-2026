fa3d12abd371187ad6d0100c71ba506dbc939d6b
Robin H <robin.hrzg@gmail.com>
2026-07-24 17:53:06 -0500

    Largest rectangle in histogram. (Monotonic stack)

---
+++ 07242026.ts
@@ -0,0 +1,34 @@
+// Largest rectangle in histogram. (Monotonic stack)
+
+function largestRectangleArea(heights: number[]): number {
+  let result = 0;
+  const stack: number[] = [];
+
+  for (let i = 0; i < heights.length + 1; i++) {
+    const height = heights[i] || 0;
+
+    while (height < heights[stack.at(-1)]) {
+      const j = stack.at(-2) ?? -1;
+
+      const x = i - j - 1;
+      const y = heights[stack.pop()];
+      const area = x * y;
+
+      result = Math.max(result, area);
+    }
+
+    stack.push(i);
+  }
+
+  return result;
+}
+
+console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]), 10);
+console.log(largestRectangleArea([2, 4]), 4);
+console.log(largestRectangleArea([1, 2, 3, 4, 5, 6]), 12);
+console.log(largestRectangleArea([6, 5, 4, 3, 2, 1]), 12);
+// https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+// heights: number[]. 1 <= heights.length <= 10^5. 0 <= heights[i] <= 10^4.
+// Return the area of the largest rectangle in the histogram.
+// - A rectangle can be made up of multiple bars.
