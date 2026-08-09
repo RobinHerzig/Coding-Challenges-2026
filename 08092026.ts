5724e32ecccf47de5bcdb3a4a8a3802a6de1ed40
Robin H <robin.hrzg@gmail.com>
2026-08-09 15:21:39 -0500

    Subsets II. (Backtracking)

---
+++ 08092026.ts
@@ -0,0 +1,32 @@
+// Subsets II. (Backtracking)
+
+function subsetsWithDup(nums: number[]): number[][] {
+  const result: number[][] = [];
+  nums.sort((a, b) => a - b);
+
+  function dfs(set: number[], i: number): void {
+    result.push([...set]);
+
+    for (let j = i; j < nums.length; j++) {
+      const candidate = nums[j];
+
+      if (j > i && candidate == nums[j - 1]) {
+        continue;
+      }
+
+      set.push(candidate);
+      dfs(set, j + 1);
+      set.pop();
+    }
+  }
+
+  dfs([], 0);
+  return result;
+}
+
+console.log(subsetsWithDup([1, 2, 2]), [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]]);
+console.log(subsetsWithDup([0]), [[], [0]]);
+// https://leetcode.com/problems/subsets-ii/
+
+// nums: number[]. 1 <= nums.length <= 10. -10 <= nums[i] <= 10.
+// Return an array of all possible subsets, including partial or empty subsets. Do not include duplicate sets.
