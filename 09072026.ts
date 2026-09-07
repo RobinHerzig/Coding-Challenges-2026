a5a309f34110836d60f5e6159ed884bc432cd34f
Robin H <robin.hrzg@gmail.com>
2026-09-07 16:36:57 -0500

    Longest consecutive sequence in unsorted array.

--- 09072026.ts
+++ 09072026.ts
@@ -1,1 +1,31 @@
-// File for Mon Sep 07 2026
+// Longest consecutive sequence in unsorted array.
+
+function longestConsecutive(nums: number[]): number {
+  const set = new Set(nums);
+  let length = 0;
+
+  for (const num of set) {
+    if (set.has(num - 1)) {
+      continue;
+    }
+
+    let i = 1;
+
+    while (set.has(num + i)) {
+      i++;
+    }
+
+    length = Math.max(length, i);
+  }
+
+  return length;
+}
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]), 4); // [1, 2, 3, 4]
+console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]), 9); // [0, 1, 2, 3, 4, 5, 6, 7, 8]
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]), 4); // [1, 2, 3, 4]
+console.log(longestConsecutive([]), 0);
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+// nums: number[]. 0 <= nums.length <= 10^5. -10^9 <= nums[i] <= 10^9. Unsorted.
+// Return the length of the longest consecutive sequence. Complexity must be O(n).
