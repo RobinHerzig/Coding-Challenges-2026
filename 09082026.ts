cb0574099e8e75e0386585fd97610c9ca44b2507
Robin H <robin.hrzg@gmail.com>
2026-09-08 12:55:43 -0500

    Find the duplicate number. (Floyd's algorithm)

--- 09082026.ts
+++ 09082026.ts
@@ -1,1 +1,32 @@
-// File for Tue Sep 08 2026
+// Find the duplicate number. (Floyd's algorithm)
+
+function findDuplicate(nums: number[]): number {
+  let result = 0;
+  let slow = nums[0];
+  let fast = nums[0];
+
+  // Find node where slow/fast pointers meet.
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow !== fast);
+
+  // Increment with node from beginning of list.
+  let slow2 = nums[0];
+
+  while (slow !== slow2) {
+    slow = nums[slow];
+    slow2 = nums[slow2];
+  }
+
+  return slow;
+}
+
+console.log(findDuplicate([1, 3, 4, 2, 2]), 2);
+console.log(findDuplicate([3, 1, 3, 4, 2]), 3);
+console.log(findDuplicate([3, 3, 3, 3, 3]), 3);
+// https://leetcode.com/problems/find-the-duplicate-number
+
+// nums: number[]. 1 <= n <= 10^5. 1 <= nums[i] <= n + 1.
+// Return the only integer that appears two or more times.
+// - Don't modify nums and use constant space.
